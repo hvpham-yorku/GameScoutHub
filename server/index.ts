bd68789988579b6d2fd7f48f2e56a1d854a490f5
theo-thanhlam <phatlam.media.vis@gmail.com>
2024-11-19 15:07:57 -0500

    Finished: create global DB variable with Singleton design

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,11 @@
+import { db } from "./db"
+
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3001
 
 
 
-app.listen(port, () => {console.log(`Server is running at ${port}`)})
+app.listen(port, () => {
+    if(db == null) console.log("Error when creating db")
+    console.log(`Server is running at ${port}`)})
