c6e39a1f29ac010f1a25b07313695e76f40c72f1
Alp Sirek <71943217+hiimangel@users.noreply.github.com>
2024-11-19 21:41:42 -0500

    A simple header addition
    A simple header component and css for it has been created

---
+++ client/src/components/header.tsx
@@ -0,0 +1,41 @@
+import React from "react"; 
+
+
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        GameScoutHub
+      </div>
+      <nav className="header-nav">
+
+        <a href="/how-to-use">How to Use</a>
+        <a href="/newsstand">Newsstand</a>
+        <a href="/about-us">Who we are</a>
+        <a href="/profile">My profile</a>
+
+      </nav>
+
+      <div className="header-icons">
+
+        <a href="/support">
+          <i className="icon-support">🙋‍♂️</i>
+        </a>
+
+        <a href="/discord">
+          <i className="icon-discord">🎮</i>
+        </a>
+
+        <a href="/settings">
+          <i className="icon-settings">⚙️</i>
+        </a>
+
+      </div>
+
+    </header>
+  );
+};
+
+export default Header;
