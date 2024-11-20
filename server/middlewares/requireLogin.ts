f2dd51ec49f1ac536e38ce0ffa19439900584371
theo-thanhlam <phatlam.media.vis@gmail.com>
2024-11-20 16:16:14 -0500

    Modify: pass user token to next function in middleware

--- server/middlewares/requireLogin.ts
+++ server/middlewares/requireLogin.ts
@@ -6,12 +6,11 @@ import {Request, Response,NextFunction} from "express"
 export function requireLogin(req:Request,res:Response,next:NextFunction){
     const token = {value:req.cookies["GSH_LGIN_TOKEN"] as string};
    
-    // if(!token.value) return res.status(401).json({msg:""})
-        jwt.verify(token.value, process.env.LOGIN_TOKEN_SECRET as string, {}, (err, info) => {
+    jwt.verify(token.value, process.env.LOGIN_TOKEN_SECRET as string, {}, (err, info) => {
             if (err) {
                 return res.status(401).json({ msg: "Invalid or expired token" });
             }
-            // req.user = info; // Optionally attach the decoded token payload to the request
+            res.locals.usersLoad = info; // Optionally attach the decoded token payload to the request
             next(); // Proceed to the next middleware/route
         });
         
