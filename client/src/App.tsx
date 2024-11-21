3876baf207367214660eed35216d6c01b80b0438
Alp Sirek <71943217+hiimangel@users.noreply.github.com>
2024-11-20 19:42:50 -0800

    I DONT KNOW WHATS HAPPENING
    WHAT THE

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import Header from "./components/header"; 
+import SlidingNews from "./components/newsList"; 
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +13,12 @@ function App() {
   return (
     <>
       <div>
+
         <Header/>
-     
+        <SlidingNews/>
 
       
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
