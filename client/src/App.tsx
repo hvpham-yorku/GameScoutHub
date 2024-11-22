93052a3095013e7519b59d695fffaae4c3c483d9
theo-thanhlam <phatlam.media.vis@gmail.com>
2024-11-21 19:22:24 -0500

    Added packages installation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,21 +11,20 @@ import AboutUs from "./pages/AboutUs";
 import ListOfNews from "./components/ListOfNews";
 import { UserContextProvider } from "./contexts/userContext";
 
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path={"/login"} element={<LoginPage />} />
-        <Route path={"/signup"} element={<SignupPage />} />
-        <Route path ={"/news"} element={<NewsListPage />}/>
-        <Route path ={"/testnews"} element = {<ListOfNews/>}/>
-        <Route path ={"/aboutus"} element={<AboutUs />}/>
-      </Route>
-
-
-
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path={"/login"} element={<LoginPage />} />
+          <Route path={"/signup"} element={<SignupPage />} />
+          <Route path={"/news"} element={<NewsListPage />} />
+          <Route path={"/testnews"} element={<ListOfNews />} />
+          <Route path={"/aboutus"} element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
